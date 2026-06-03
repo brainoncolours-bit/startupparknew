8a60053fbf4d21db7d369fd8f0ec7e8f986e5abe
ABHIRAMI KRISHNA <abhiramikrishna796@gmail.com>
2026-06-03 22:03:58 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-import { defineConfig } from 'vite'
+import { createLogger, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const logger = createLogger()
+const loggerWarn = logger.warn
+
+logger.warn = (msg, options) => {
+  if (typeof msg === "string" && msg.includes("[PLUGIN_TIMINGS]")) return
+  loggerWarn(msg, options)
+}
+
 export default defineConfig({
   plugins: [react()],
+  customLogger: logger,
   build: {
     chunkSizeWarningLimit: 1200,
   },
